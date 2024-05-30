f81b9aaca0718320497991174b12300ea53f13dc
CnmDev07 <cnavasmesa@gmail.com>
2024-05-30 15:54:59 +0200

    1

--- src/App.java
+++ src/App.java
@@ -2,31 +2,8 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
-        int opcionMenu;
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Que quieres hacer??");
-        System.out.println("MENU:\n[1]Mostrar Cartelera");
-        System.out.println("[2]Comprar entrada");
-        opcionMenu= sc.nextInt();
-
-        switch (opcionMenu) {
-            case 1:
-
-                break;
-            case 2:
-
-                break;
-
-            case 3:
-
-                break;
-
-            default:
-                break;
-        }
-        sc.close();
+        
+        
 
     }
 }
